82b4069dce20f668b1cbdd8b39a82cac1e5e8dae
Huan Li <yrlihuan@gmail.com>
2013-06-24 12:29:43 +0800

    google interview 3&4

---
+++ google/20130619.c
@@ -0,0 +1,19 @@
+第三面
+
+英文面，先介绍下自己
+
+开放性问题。用户在搜索框内输入一个地址，类似"xxx Street, Haidian, Beijing", 如何确知它是否有效？
+写代码。。。。
+用户输入"Bejing Duck"，如何识别这样一个非地址字符串
+用户输入"Chaoyang Men"，如何识别这是北京而非沈阳
+
+第四面
+
+字符串相减
+
+一个数据中心jobs的log类似 [user, start, end, cpu, memory, io]
+如何获得某个用户使用的最大cpu
+若用户的cpu使用是线性的，（每个user给出开始cpu和结束cpu两个值）
+获取每个用户的最大cpu
+
+
